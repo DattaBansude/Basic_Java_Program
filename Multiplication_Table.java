83922804c605d2d41f2099737b728c517aa9e8e6
Datta Bansude <103624892+DattaBansude@users.noreply.github.com>
2024-05-21 16:10:42 +0530

    Add files via upload

--- Multiplication_Table.java
+++ Multiplication_Table.java
@@ -1,18 +1,17 @@
-package com.labassignment;
+package com.module8;
 
 import java.util.Scanner;
 
 public class Multiplication_Table {
 
-	public static void main(String[] args) {
-		int result;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter any Number: ");
-		int input= sc.nextInt();
-		
-		for (int i = 1; i <= 10; i++) {
-			result = input*i;
-			System.out.println(input+"*"+i+" = "+result);
-		}
-	}
+	 public static void main(String[] args) {  
+	  Scanner sc = new Scanner(System.in);  
+	  System.out.print("Enter a number: "); 
+	  int num = sc.nextInt();
+	
+	  for (int i = 1; i <= 10; i++) {
+		  
+	     System.out.println(num + " x " + i + " = " + num * i );
+	  }
+	 }
 }
